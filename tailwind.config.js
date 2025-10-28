6bf574c5d8dcd55578d9770087d0e7e9feb2d981
callmeAyanda <a22.makhubu@gmail.com>
2025-10-28 12:10:58 +0200

    more changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
-  purge: [
+  content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
